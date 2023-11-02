19cc163eb40ada16627f20a1d770ab635cfe769a
ragul.d <ragul.d@sysveda.co.in>
2023-11-02 21:28:25 +0530

    sample-web-app

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:11
+EXPOSE 8080
+ADD target/learn-github-actions.jar learn-github-actions.jar 
+ENTRYPOINT ["java","-jar","/learn-github-actions.jar"]
---
+++ src/main/java/com/github/actions/MyRestController.java
@@ -0,0 +1,22 @@
+package com.github.actions;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping(value = "/")
+public class MyRestController {
+
+
+
+	@GetMapping(value = "/")
+	public String name() {
+
+		return "Product Added";
+
+	}
+
+}
